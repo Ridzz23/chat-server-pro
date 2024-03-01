531e69110d1e4d1671888f89ca0a2fc8d21da569
Ridhi Srikanth <rsrikanth@ieng6-202.ucsd.edu>
2024-02-29 17:48:07 -0800

    Fixed bugs

--- SemanticAnalysisHandlerTests.java
+++ SemanticAnalysisHandlerTests.java
@@ -42,5 +42,39 @@ public class SemanticAnalysisHandlerTests {
     h.handleRequest(input2);
     h.handleRequest(input3);
     assertEquals(expected, h.handleRequest(input4));
+  
+  }
+
+    @Test
+    public void handleRequestSemanticAnalysisUserNew() throws Exception {
+    ChatHandler h = new ChatHandler();
+    String url1 = "http://localhost:4000/chat?user=onat&message=😂";
+    String url2 = "http://localhost:4000/chat?user=edwin&message=doggy🥹!!!";
+    String url3 = "http://localhost:4000/chat?user=onat&message=TGIThanksgiving";
+    String url4 = "http://localhost:4000/semantic-analysis?user=onat";
+
+    URI input1 = new URI(url1);
+    URI input2 = new URI(url2);
+    URI input3 = new URI(url3);
+    URI input4 = new URI(url4);
+    String expected = "onat: 😂 This message has a LOL vibe.\n\nedwin: doggy🥹!!! This message has a awwww vibe. This message ends forcefully.\n\nonat: TGIThanksgiving\n\n";
+
+    h.handleRequest(input1);
+    h.handleRequest(input2);
+    h.handleRequest(input3);
+    assertEquals(expected, h.handleRequest(input4));
+  }
+
+
+    @Test
+    public void handleRequestSemanticAnalysisManyEmojis() throws Exception {
+    ChatHandler h = new ChatHandler();
+    String url1 = "http://localhost:4000/chat?user=onat&message=🥹😂";
+
+    URI input1 = new URI(url1);
+    String expected = "onat: 🥹! 😂 This message has a awwww vibe.This message has a LOL vibe.";
+
+    h.handleRequest(input1);
+    assertEquals(expected, h.handleRequest(input1));
   }
 }
